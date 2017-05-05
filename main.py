41feb287521f76e582fbce232aa44d17f7e78a7f
Yuri Sturkenboom <y.a.sturkenboom@gmail.com>
2017-05-05 20:45:45 +0200

    Changed to use all data and added helpful comments.

--- main.py
+++ main.py
@@ -5,10 +5,9 @@
 # - http://scikit-learn.org/stable/tutorial/statistical_inference/
 # supervised_learning.html
 
-# TODO: add all the data (fix illegal character errors)
 # TODO: allow piping of text to the script to classify
 # TODO: output human-readable labels
-# TODO: buy a faster machine
+# TODO: buy a faster machine so training doesn't take forever
 
 import nltk
 from lxml import etree
@@ -18,13 +17,13 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-classified_dir = './data/bbc/bbc.classes_shorts'
+classified_dir = './data/bbc/bbc.classes'
 data_main_dir = './data/bbc/'
-# data_sub_dirs = ['business', 'entertainment', 'politics', 'sport', 'tech']
-data_sub_dirs = ['business', 'entertainment']
+data_sub_dirs = ['business', 'entertainment', 'politics', 'sport', 'tech']
+#data_sub_dirs = ['business', 'entertainment']
 
 
-# process classes data into target vector
+# process classes data file into target vector
 y = []
 f = open(classified_dir)
 for line in f:
@@ -34,16 +33,21 @@ for line in f:
 all_tokens = set()
 
 # The term frequencies of each article will serve as features for
-# machine learning. 
+# machine learning. Below we generate them per article.
 freqdists = []
 for data_sub_dir in data_sub_dirs:
 	for filename in os.listdir(data_main_dir + data_sub_dir + '/'):
-		f = io.open(data_main_dir + data_sub_dir + '/' + filename, encoding='utf-8')
+		f = io.open(data_main_dir + data_sub_dir + '/' + filename, encoding='latin-1')
 		text = f.read()
 
+		# Turn text from an article into tokens
 		tokens = nltk.word_tokenize(text.lower())
 
+		# Update the set of all tokens (ignoring duplicates)
 		all_tokens.update(tokens)
+
+		# Use nltk to get the frequency per term, and save this in
+		# a list
 		freqdists.append(dict(nltk.FreqDist(tokens)))
 
 # First an empty base vector is created, all terms occur zero times.
@@ -51,10 +55,13 @@ base_vector = {key: 0 for key in all_tokens}
 
 # Now, expand all term frequency dicts for each article to include
 # all tokens, so we can compare them meaningfully.
-
+# This is done by taking the base vector and updating the term
+# frequencies that were found to the correct number.
 X = []
 
 # copy() because of weird Python referencing stuff
+# The following code explains some unexpected (for me) behavior of
+# the dict.update function
 # a = {'a': 0, 'b': 0, 'c':0}
 # b = {'b': 1}
 # z = a.copy()
@@ -66,10 +73,14 @@ for x in freqdists:
 	new_vec = [new_vec[key] for key in sorted(new_vec)]
 	X.append(new_vec)
 
+# Make numpy array for better sklearn integration
 X = np.array(X)
 y = np.array(y)
 
+# Make sure results are replicable
 np.random.seed(0)
+
+# Randomize train and test sets
 indices = np.random.permutation(len(X))
 X_train = X[indices[:-10]]
 y_train = y[indices[:-10]]
@@ -79,6 +90,7 @@ y_test  = y[indices[-10:]]
 # Create and fit a nearest-neighbor classifier
 knn = KNeighborsClassifier()
 knn.fit(X_train, y_train)
+
 result = knn.predict(X_test)
 
 # Print results! :)
